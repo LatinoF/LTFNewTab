22ac2a08207821354d18b93317cd7cbf4eca77d0
LatinoF <latinof69@usa.com>
2021-08-24 09:44:44 +0200

    Changed layout, updated.

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-const f = document.getElementById('form');
-const q = document.getElementById('query');
+const F = document.getElementById('Form');
+const Q = document.getElementById('Query');
 const searchengine = 'https://duckduckgo.com/?q=site%3A+';
-var site = 'fanart.tv';
+var Site = 'fanart.tv';
 
-function submitted(event) {
+function Submitted(event) {
   event.preventDefault();
-  const url = searchengine + site + '+' + q.value;
+  const url = searchengine + Site + '+' + Q.value;
   const win = window.open(url,"_self");
 }
 
-f.addEventListener('submit', submitted);
+F.addEventListener('submit', Submitted);
 
-  function switchVisible() {
-    	var div1=document.getElementById('Div1');
-    	var div2=document.getElementById('Div2');
-    	
-      if (div1 !== undefined && div2 !== undefined) {
-    	  div1.style.display = div2.style.display === '' ? 'none' : div2.style.display === 'none' ? 'none' : 'block';
-    	  div2.style.display = div1.style.display === 'block' ? 'none' : 'block';
-      }
-    }
+function switchDOGE(){
+  document.getElementById("BTC").style.display = "none";
+  document.getElementById("ETH").style.display = "none";
+  document.getElementById("DOGE").style.display = "block";
+}
+
+function switchBTC(){
+  document.getElementById("DOGE").style.display = "none";
+  document.getElementById("ETH").style.display = "none";
+  document.getElementById("BTC").style.display = "block";
+}
+
+function switchETH(){
+  document.getElementById("DOGE").style.display = "none";
+  document.getElementById("BTC").style.display = "none";
+  document.getElementById("ETH").style.display = "block";
+}
+
