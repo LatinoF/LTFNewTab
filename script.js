9ed360ded8e17c352b20f4e436837210f3b988f1
LatinoF <latinof69@usa.com>
2021-08-26 13:23:41 +0200

    Updated

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 const F = document.getElementById('Form');
 const Q = document.getElementById('Query');
-const searchengine = 'https://duckduckgo.com/?q=site%3A+';
-var Site = 'fanart.tv';
+const searchengine = 'https://www.google.com/search?q=';
+var Site = '';
 
 function Submitted(event) {
   event.preventDefault();
@@ -33,3 +33,45 @@ document.getElementById('NoDrag1').ondragstart = function() { return false; };
 document.getElementById('NoDrag2').ondragstart = function() { return false; };
 document.getElementById('NoDrag3').ondragstart = function() { return false; };
 
+document.onclick = hideMenu;
+document.onauxclick = hideMenu;
+  
+function hideMenu() {
+  document.getElementById("contextMenu").style.display = "none"
+}
+  
+document.getElementById("EngineSwapper").oncontextmenu = function rightClick(e) {
+  e.preventDefault();
+  if (document.getElementById("contextMenu").style.display == "block"){
+    hideMenu();
+  } else {
+      var Menu = document.getElementById("contextMenu")
+      Menu.style.display = 'block';
+      Menu.style.left = e.pageX + "px";
+      Menu.style.top = e.pageY + "px";
+    }
+  }
+
+window.onload = engineCheck();
+
+function engineCheck(){
+  if(localStorage.getItem('engine') == null && localStorage.getItem('engine') === 'Google'){
+    swapGoogle();
+  } else{
+    swapTV();
+  }
+}
+
+function swapGoogle(){
+  Site = '';
+  localStorage.setItem('engine', 'Google');
+  document.getElementById("aGoogle").style.color = "red";
+  document.getElementById("aTV").style.color = "black";
+}
+
+function swapTV(){
+  Site = 'site%3Afanart.tv';
+  localStorage.setItem('engine', 'Fanart');
+  document.getElementById("aTV").style.color = "red";
+  document.getElementById("aGoogle").style.color = "black";
+}
